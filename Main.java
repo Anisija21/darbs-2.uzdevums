7702952c6a3cc1fe3cf87a5cbac882e0d2a32f33
Anisija <anisija8@inbox.lv>
2021-02-10 19:43:32 +0000

    pievienots

---
+++ Main.java
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>The + Operator</h2>
+
+<p id="demo"></p>
+
+<script>
+linoleja_cena x = 10;
+telpas_izmeri y = 9;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
+
